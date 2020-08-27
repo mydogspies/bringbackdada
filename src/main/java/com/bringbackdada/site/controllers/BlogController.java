194be0ec3eb8aa3c6488d1a50932d187d29e9bee
Peter Mankowski <mydogspies@zoho.com>
2020-08-27 22:48:20 +0200

    worked on the blog page roll design

--- src/main/java/com/bringbackdada/site/controllers/BlogController.java
+++ src/main/java/com/bringbackdada/site/controllers/BlogController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * The controller for the blog page
- * @author mydogspies@zoho.com
+ *
  * @since 0.0.1
  */
 @Controller
@@ -34,58 +34,56 @@ public class BlogController {
 
     private final Logger logger = LoggerFactory.getLogger(BlogController.class);
 
-    @RequestMapping(value={"blog","blog.html"}, method= RequestMethod.GET)
-    public String getBlog(Model model){
+    @RequestMapping(value = {"blog", "blog.html"}, method = RequestMethod.GET)
+    public String getBlog(Model model) {
 
         Set<Blog> blogSet = blogService.findAll();
         List<Blog> sortedBlogList = sortBlog(blogSet);
-        List<Map<String, Object>> thymeleafOutput = formattedBlogOutput(sortedBlogList);
 
-        Map<String, Object> firstEntry = thymeleafOutput.get(0);
+        List<Map<String, Object>> thymeFormattedOutput = thymeOutput(sortedBlogList);
+
+        Map<String, Object> firstEntry = thymeFormattedOutput.get(0);
         model.addAttribute("firstBlog", firstEntry);
-        sortedBlogList.remove(0);
+        thymeFormattedOutput.remove(0);
 
-        model.addAttribute("restBlogs", sortedBlogList);
-
+        model.addAttribute("restBlogs", thymeFormattedOutput);
         model.addAttribute("title_text", "Bringbackdada - BLOG");
 
         logger.info("--> Calling blog.html");
         return "blog";
     }
 
-    /**
-     * Takes all the data and makes it into a hashmap ready-formatted to be output by Thymeleaf.
-     * @param sortedBlogs
-     * @return a list of HashMaps, each being a Blog entry formatted for Thymeleaf.
-     */
-    private List<Map<String, Object>> formattedBlogOutput(List<Blog> sortedBlogs) {
+    private List<Map<String, Object>> thymeOutput(List<Blog> blogList) {
 
-        List<Map<String, Object>> output = new ArrayList<>();
-        Map<String, Object> outputMap = new HashMap<>();
+        List<Map<String, Object>> outputList = new ArrayList<>();
 
-        for (Blog blog : sortedBlogs) {
+        for (Blog blog : blogList) {
+            Map<String, Object> outputMap = new HashMap<>();
+
             outputMap.put("entryName", blog.getEntryName());
-            outputMap.put("contentSnippet", blog.getContentSnippet());
             outputMap.put("entryContent", blog.getEntryContent());
-            outputMap.put("creator", blog.getCreator().getName());
-            outputMap.put("category", blog.getCategory());
+            outputMap.put("contentSnippet", blog.getContentSnippet());
+            outputMap.put("creator", blog.getCreator());
 
+            // TODO implement a format of Instant
             Instant instant = blog.getMilliseconds();
-            outputMap.put("time", "INSTANT");
+            String datetime = DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("UTC")).format(instant);
+            outputMap.put("time", datetime);
 
             if (!blog.getBlogImageId().equals(1L)) {
-                Content content  = contentService.findById(blog.getBlogImageId());
-                outputMap.put("imgUrl", content.getContentUrl() + content.getContentFile());
+                Long imgId = blog.getBlogImageId();
+                Content content = contentService.findById(imgId);
+                outputMap.put("imgUrl", "/images" + content.getContentUrl() + "/" + content.getContentFile());
             } else {
-                outputMap.put("imgUrl", "#");
+                outputMap.put("imgUrl", "n/a");
             }
 
-            output.add(outputMap);
+            outputList.add(outputMap);
         }
-
-        return output;
+        return outputList;
     }
 
+    // TODO verify sort order and functionality with real life data
     private List<Blog> sortBlog(Set<Blog> blog) {
         List<Blog> blogList = new ArrayList<>(blog);
         Comparator<Blog> timeSorter = Comparator.comparing(Blog::getMilliseconds);
